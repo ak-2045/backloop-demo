427c99eb4e6521ad77a44aacaed5ce1cdaee03e5
ak-2045 <168424158+ak-2045@users.noreply.github.com>
2025-06-28 16:31:12 +0530

    added logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ export const metadata: Metadata = {
   title: 'BackLoop Demo',
   description: 'Returns Made Easier',
   generator: 'Akmal',
+  icons: {
+    icon: [
+      { url: 'https://cdn-icons-png.freepik.com/512/2819/2819071.png', type: 'image/png' },
+    ],
+  },
 }
 
 export default function RootLayout({
